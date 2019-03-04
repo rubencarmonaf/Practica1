bd189de115892344690445a80d6d50c5c79835c5
Ruben <rubencarmonaf@gmail.com>
2019-03-04 21:49:03 +0100

    cambios en dardealtallamada

--- src/main/java/TestFacturacion.java
+++ src/main/java/TestFacturacion.java
@@ -81,7 +81,9 @@ public class TestFacturacion {
         System.out.println("Numero llamado: ");
         String num_llamado = sc.nextLine();
         Calendar fecha = Calendar.getInstance(Locale.getDefault());
-        Llamadas llamada = new Llamadas(num_llamado, fecha, fecha.getTime(), 1);
+        System.out.println("Introducir tiempo llamada: ");
+        double tiempo_llamada = sc.nextDouble();
+        Llamadas llamada = new Llamadas(num_llamado, fecha, fecha.getTime(), tiempo_llamada);
         System.out.print("Cliente que ha realizado la llamada (Introducir NIF): ");
         String nif_cliente = sc.nextLine();
         listaclientes.get(nif_cliente).añadirllamada(llamada);
