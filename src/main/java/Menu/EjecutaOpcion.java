87426e98330a1dbc21980fb06692a4556e43e6c5
Ruben <rubencarmonaf@gmail.com>
2019-03-25 19:21:09 +0100

    Added Execption if factura created

--- src/main/java/Menu/EjecutaOpcion.java
+++ src/main/java/Menu/EjecutaOpcion.java
@@ -1,6 +1,7 @@
 package Menu;
 
 import Clientes.Cartera;
+import Excepciones.ExistingBillException;
 import Excepciones.ExistingClientException;
 import Excepciones.NonExistingClientException;
 
@@ -10,5 +11,5 @@ import java.util.Scanner;
 public interface EjecutaOpcion {
     Scanner TECLADO = new Scanner(System.in);
     PrintStream CONSOLA =  System.out;
-    void ejecuta(Cartera cartera) throws ExistingClientException, NonExistingClientException;
+    void ejecuta(Cartera cartera) throws ExistingClientException, NonExistingClientException, ExistingBillException;
 }
--- src/main/java/Menu/NuevaFactura.java
+++ src/main/java/Menu/NuevaFactura.java
@@ -1,17 +1,19 @@
 package Menu;
 
 import Clientes.Cartera;
+import Excepciones.ExistingBillException;
 import Facturas.Factura;
 import Llamadas.Llamada;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import static Clientes.Cartera.sc;
 
 
 public class NuevaFactura implements EjecutaOpcion {
     @Override
-    public void ejecuta(Cartera cartera) {
+    public void ejecuta(Cartera cartera) throws ExistingBillException {
         System.out.print("Introduce el NIF del cliente: ");
         String nif = sc.nextLine();
         double tiempo_tot = 0;
@@ -22,12 +24,24 @@ public class NuevaFactura implements EjecutaOpcion {
         double tot_factura = tiempo_tot * tarifa;
         System.out.print("Código Factura: ");
         int codigo =  sc.nextInt();
-        LocalDate fecha_emision = LocalDate.now();
-        //Cambiar fecha fin facturación
-        LocalDateTime mes_ini = LocalDateTime.now();
-        LocalDateTime mes_fin = LocalDateTime.now();
-        LocalDateTime [] facturacion = {mes_ini, mes_fin};
-        Factura factura = new Factura(codigo, Cartera.listaclientes.get(nif).tarifa, fecha_emision, facturacion, tot_factura);
+        if (Cartera.listaclientes.get(nif).Listafacturas.containsKey(codigo))
+            throw new ExistingBillException();
+        System.out.print("Insertar dia facturacion: ");
+        int day_start = sc.nextInt();
+        System.out.print("Insertar mes facturacion:  ");
+        int month_start = sc.nextInt();
+        System.out.print("Insertar año facturacion: ");
+        int year_start = sc.nextInt();
+        Calendar mes_ini = new GregorianCalendar(year_start, month_start -1, day_start);
+        System.out.print("Insertar dia fin facturacion: ");
+        int day_end = sc.nextInt();
+        System.out.print("Insertar mes fin facturacion: ");
+        int month_end = sc.nextInt();
+        System.out.print("Insertar año fin facturacion: ");
+        int year_end = sc.nextInt();
+        Calendar mes_fin = new GregorianCalendar(year_end, month_end - 1, day_end);
+        Calendar [] facturacion = {mes_ini, mes_fin};
+        Factura factura = new Factura(codigo, Cartera.listaclientes.get(nif).tarifa, mes_ini, facturacion, tot_factura);
         Cartera.listaclientes.get(nif).anadrifactura(codigo, factura);
         System.out.println();
     }
